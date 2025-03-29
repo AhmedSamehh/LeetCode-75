92ef84f23102b012a5d3ab41d602fa1a81ad3222
Ahmad Sameh <41856063+AhmedSamehh@users.noreply.github.com>
2025-03-29 16:45:02 +0200

    [151] [Time Beats: 86.42%] [Memory Beats: 71.38%] - LeetPush

---
+++ Reverse-Words-in-a-String.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String reverseWords(String s) {
+        String[] l = s.split(\\\\\s+\);
+        StringBuilder sb = new StringBuilder();
+        for(int i=l.length-1; i>=0; i--){
+            sb.append(l[i].trim());
+            sb.append(\ \);
+        }
+        return sb.toString().trim();
+    }
+}
